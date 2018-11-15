3704340e2e198fd47503ee7ee77a0a9688a13815
yuminchoi <chldbals219@naver.com>
2018-11-15 16:05:41 +0900

    calculation program

--- main.c
+++ main.c
@@ -2,18 +2,58 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char *proverb="All that glisters is not gold.";
 
-void setPointer(char **q)
+int addfunc(int a, int b)
 {
-	*q=proverb;
+	return a+b;
+}
+
+int subfunc(int a, int b)
+{
+	return a-b;
+}
+
+int mulfunc(int a, int b)
+{
+	return a*b;
+}
+
+int divfunc(int a, int b)
+{
+	return a/b;
 }
 
 
 int main(int argc, char *argv[]) {
-	char *p="zzz";
-	setPointer(&p);
-	printf("%s\n", p);
+	int num1, num2; 
+	char op;
+	int (*calcfunc)(int, int);
 	
+	printf("input calculation :");
+	scanf("%d%c%d", &num1, &op, &num2);
+	
+	switch(op)
+	{
+		case'+':
+			calcfunc=addfunc;
+			break;
+		
+		case'-':
+			calcfunc=subfunc;
+			break;
+			
+		case'*':
+			calcfunc=mulfunc;
+			break;
+			
+		case'/':
+			calcfunc=divfunc;
+			break;
+	}
+	
+	
+	printf("result: %i\n", calcfunc(num1,num2)); 
+	
+
 	return 0;
 }
